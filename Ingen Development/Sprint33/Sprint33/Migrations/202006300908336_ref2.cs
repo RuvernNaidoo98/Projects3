747ec85c28abace8ebfaa923c2e45f1f1ab95c82
RuvernNaidoo98 <62290005+RuvernNaidoo98@users.noreply.github.com>
2020-06-30 17:59:36 +0200

    Katelyn Referral Update
    Updated version (No GUI control for Date)
    Validations completed

---
+++ Ingen Development/Sprint33/Sprint33/Migrations/202006300908336_ref2.cs
@@ -0,0 +1,16 @@
+namespace Sprint33.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ref2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Ingen Development/Sprint33/Sprint33/Sprint33/Migrations/202006301506311_referralsnew.cs
@@ -0,0 +1,16 @@
+namespace Sprint33.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class referralsnew : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
